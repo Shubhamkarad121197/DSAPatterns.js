130d0f52401a8c49bcce96cc9f0c6eb9869d636f
shubham karad <shubham.karad12@gmail.com>
2025-10-15 01:01:50 +0530

    Two pointer added

---
+++ DSA_General_Question/TwoPointers/reverseString.js
@@ -0,0 +1,14 @@
+function reverseString(str){
+    let arr=str.split('')
+    let left=0,right=arr.length-1
+    while(left<right){
+        [arr[left],arr[right]]=[arr[right],arr[left]]
+        left++;
+        right--;
+    }
+
+    return arr.join('')
+}
+
+
+console.log(reverseString("hello")); // Output: "olleh"
