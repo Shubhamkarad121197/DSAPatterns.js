ceb25dc970fabf49eaa01dfaa8d3b2547a3e1c60
shubham karad <shubham.karad12@gmail.com>
2025-10-26 11:58:20 +0530

    longestSubstringwithKDistinctChar

---
+++ DSA_General_Question/slidingWindow/logestSubstringWithKDistinctChar.js
@@ -0,0 +1,67 @@
+// Problem Description
+// Given a string, find the length of the longest substring that contains at most K distinct characters.
+
+// Note:
+
+// Uppercase and Lowercase characters should be considered as different characters.
+
+// There can be numbers and special characters as well.
+
+// Input format
+// There are 2 lines of input.
+
+// First-line contains two space-separated integers representing the value of N and K.
+
+// The next line contains the string S of length N.
+
+// Output format
+// The length of the longest substring T that contains at most K distinct characters.
+
+// Constraints
+// 1 <= N <= 10^5
+
+// 0 <= K <= 256
+
+// Sample Input 1
+// 5 3
+
+// abacd
+
+// Sample Output 1
+// 4
+
+// Explanation 1
+// The longest substring that has 3 distinct characters, "abac", is of length 4.
+
+// Sample Input 2
+// 6 3
+
+// Xyyzya
+
+// Sample Output 2
+// 5
+
+// Explanation
+// There are 2 substrings, "Xyyzy" and “yyzya” which are the longest ones having 3 distinct characters and their length is 5.
+
+
+//1. Brute Force Solution
+function lengthOfLongestSubstringKDistinct(s, k) {
+    let maxLen=0;
+    debugger;
+    for(let i=0;i<s.length;i++){
+        let distinct=new Set();
+        for(j=i;j<s.length;j++){
+            distinct.add(s[j])
+            if(distinct.size<=k){
+                maxLen=Math.max(maxLen,j-i+1)
+            }
+            else{
+                break;
+            }
+        }
+    }
+    return maxLen;
+}
+
+console.log(lengthOfLongestSubstringKDistinct("eceba", 2)); // Output: 3
